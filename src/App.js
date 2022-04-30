563a259f4034ae67d8869dd2beb54835c5981b32
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-04-30 14:55:53 +0300

    Add fetch logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState, useCallback } from 'react';
 
 import Container from './components/UI/Container/Container';
 import AdviceBox from './components/AdviceBox/AdviceBox';
 
 function App() {
+  const [advice, setAdvice] = useState({});
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchAdvice = useCallback(async () => {
+    try {
+      setFetchError(null);
+      setIsLoading(true);
+
+      const response = await fetch('https://api.adviceslip.com/advice');
+
+      if (!response.ok)
+        throw new Error(
+          'There was a problem with the server! Please try again!'
+        );
+
+      const data = await response.json();
+
+      setAdvice({ ...data.slip });
+    } catch (err) {
+      setFetchError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchAdvice();
+  }, [fetchAdvice]);
+
   return (
     <Fragment>
       <Container>
-        <AdviceBox />
+        <AdviceBox
+          isLoading={isLoading}
+          error={fetchError}
+          adviceData={advice}
+          onButtonClick={fetchAdvice}
+        />
       </Container>
     </Fragment>
   );
